6264ed6740820c340a4e67857491a06166789506
Samridhan2005 <samridhanselvan@gmail.com>
2025-06-06 11:52:05 +0530

    final portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+  strings: ['Full Stack Developer', 'Problem Solver', 'Tech Enthusiast'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
